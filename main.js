f00a655f5bafb1b76f2a0fcc6a365c91dfdae255
mdizam05 <166032761+mdizam05@users.noreply.github.com>
2024-04-04 19:38:23 +0500

    ATM

---
+++ main.js
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "userId",
+        message: "kindly enter your Id: "
+    },
+    {
+        type: "number",
+        name: "userPin",
+        message: "kindly enter your Pin: "
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["Current", "Savings"],
+        message: "kindly enter your account type: "
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["FastCash", "withdraw"],
+        message: "Select your transaction type: ",
+        when(answer) {
+            return answer.accountType;
+        }
+    },
+    {
+        type: "list",
+        name: "amount",
+        choices: [1000, 2000, 5000, 10000],
+        message: "Select your amount: ",
+        when(answer) {
+            return answer.transactionType == "FastCash";
+        },
+    }, {
+        type: "number",
+        name: "amount",
+        message: "Enter your amount: ",
+        when(answer) {
+            return answer.transactionType == "withdraw";
+        }
+    },
+]);
+if (answer.userId && answer.userPin) {
+    const balance = Math.floor(Math.random() * 1000000);
+    console.log(balance);
+    const EnteredAmount = answer.amount;
+    if (balance >= EnteredAmount) {
+        const remaining = balance - EnteredAmount;
+        console.log("Your remaining balance is ", remaining);
+    }
+    else {
+        console.log("Insufficient Amount");
+    }
+}
